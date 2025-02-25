12e0dc5d0291dc24117bab216da0449960c943d0
Theo Hadj <theo.hadja@gmail.com>
2025-02-25 12:31:02 +0100

    .

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3Validator;
+
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Recaptcha3Validator $recaptcha3Validator): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($recaptcha3Validator->getLastResponse()->getScore() < 0.5) {
+                    $msg = "L'utilisateur est un bot";
+                    $notice = "danger";
+                    dump("CAPTTCHATTLKDJSF");
+                    return $msg;
+            }
+            else{
+                dump("JHSQDJKHDSQBDBBDBDBD MANQUANT");
+            }
+
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,15 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -32,12 +36,26 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\Length(
+        min: 4,
+        max: 50,
+        minMessage: 'Le nom doit contenir au moins {{ limit }} caractères ',
+        maxMessage: 'Le nom ne doit pas dépasser {{ limit }} caractères',
+        )]
     private ?string $nom = null;
 
+
     #[ORM\Column(length: 50)]
+    #[Assert\Length(
+        min: 4,
+        max: 50,
+        minMessage: 'Le prenom doit contenir au moins {{ limit }} caractères ',
+        maxMessage: 'Le prenom ne doit pas dépasser {{ limit }} caractères',
+        )]
     private ?string $prenom = null;
 
     #[ORM\Column]
+    #[Assert\Type('bool')]
     private ?bool $activate = null;
 
     public function __construct()
